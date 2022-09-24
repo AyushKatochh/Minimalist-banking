20ee1c8e81fd5d5820e5050e7088a74c31e812bb
AyushKatochh <katochayushhp@gmail.com>
2022-09-24 15:03:36 +0530

    minimalist banking

---
+++ script.js
@@ -0,0 +1,133 @@
+'use strict';
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+//////////////////////////////////////////////////
+
+// Selecting elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('.button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+//Creating and Inserting ELements
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+ 
+message.innerHTML = 'We use cookies for improved functionality and analytics  <button class="btn btn--close-cookie">Got it!</button>';
+
+header.append(message);
+
+
+document.querySelector('.btn--close-cookie').addEventListener('click', () => {
+  //message.remove();
+  message.parentElement.removeChild(message);
+})
+
+/*
+//Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).height);
+
+message.style.height = Number.parseFloat(getComputedStyle(message).height, 10)+ 30 + 'px';
+
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+
+
+logo.alt = 'Beautiful minimalist logo';
+
+//Non-standard
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+
+// It will give absolute path
+console.log(logo.src);
+
+console.log(logo.getAttribute('src'));
+
+const link  = document.querySelector('.nav__link-btn');
+console.log(link.href);
+
+//Data attributes
+logo.classList.add('c', 'j');
+logo.classList.remove('c','j');
+logo.classList.toggle('c');
+logo.classList.contains('c');
+
+//Don't use
+//logo.className = 'ayush'
+
+*/
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', (e) => {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll X/Y', window.pageXOffset, window.pageYOffset);
+
+  console.log('Current height and width', document.documentElement.clientHeight, document.documentElement.clientWidth);
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behaviour: 'smooth'
+  // })
+
+  section1.scrollIntoView({behaviour: 'smooth'});
+
+})
